3fb094f2120faebec1183516e38638326c5ce9a9
Andrés Colón <mindware@Dendrite.local>
2020-05-01 13:06:17 -0400

    Updating default rake task to show available tasks.

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,8 @@
-task default: %w[start]
+require_relative 'config/application.rb'
+require_all 'tasks'
 
-task :start do 
-  ruby 'config/application.rb'
+#task default: %w[start]
+task :default do 
+  puts "Which task would you like to do? (see ./tasks/)"
+  system("rake -T -A")
 end
---
+++ tasks/start.rb
@@ -0,0 +1,3 @@
+task :start do
+   ruby 'config/application.rb'
+end
